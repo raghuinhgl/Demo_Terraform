68545e016c2d7577ac348a5c368e359732750d08
raghuinhgl <76654262+raghuinhgl@users.noreply.github.com>
2022-02-22 09:20:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_security_group" "sg" {
       to_port          = 0
       protocol         = "-1"
       cidr_block       = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
+      ipv6_cidr_blocks = null
       prefix_list_ids  = null
       security_groups  = null
       self             = null
